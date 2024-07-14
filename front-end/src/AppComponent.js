36189d78eb806070f3d6a21e95d5e0697815082a
Nguyen Thanh Thuy <ntthuy3.it@gmail.com>
2024-07-15 02:29:19 +0700

    feat: label tooltip for menu

--- front-end/src/AppComponent.js
+++ front-end/src/AppComponent.js
@@ -1,5 +1,7 @@
-import { lazy } from 'react';
+import {lazy} from 'react';
 import {HomeOutlined, SettingOutlined} from "@ant-design/icons"
+import {Tooltip} from "antd";
+import LabelTooltipForMenuItem from "./layouts/LabelTooltipForMenuItem";
 
 // Lazy imports
 const Home = lazy(() => import('./Home'));
@@ -9,6 +11,7 @@ const MemberListView = lazy(() => import('./pages/member/view/MemberListView'));
 const MetaCataloListView = lazy(() => import('./pages/metacatalo/veiw/MetaCataloListView'));
 const CataloListView = lazy(() => import('./pages/metacatalo/veiw/CataloListView'));
 const BookListView = lazy(() => import('./pages/book/view/BookListView'));
+const BorrowListView = lazy(() => import('./pages/borrow/view/BorrowListView'));
 const ImportBookListView = lazy(() => import('./pages/import-book/view/ImportBookListView'));
 
 // Exporting components and lazy imports
@@ -17,15 +20,15 @@ export const routeComponents = [
     component: Home,
     key: '/',
     name: 'home',
-    label: 'Home',
-    icon: <HomeOutlined />,
+    label: <LabelTooltipForMenuItem label={'Home'} />,
+    icon: <HomeOutlined/>,
     displayIndex: '0'
   },
   {
     component: AuthorListView,
     key: '/author',
     name: 'author',
-    label: 'Tác giả',
+    label: <LabelTooltipForMenuItem label={'Tác giả'} />,
     icon: <span><i className="fa-solid fa-user-pen"></i></span>,
     displayIndex: '1'
   },
@@ -33,7 +36,7 @@ export const routeComponents = [
     component: PublisherListView,
     key: '/publisher',
     name: 'publisher',
-    label: 'Nhà phát hành',
+    label: <LabelTooltipForMenuItem label={'Nhà phát hành'} />,
     icon: <span><i className="fa-solid fa-swatchbook"></i></span>,
     displayIndex: '2'
   },
@@ -41,14 +44,14 @@ export const routeComponents = [
     component: MemberListView,
     key: '/member',
     name: 'member',
-    label: 'Thành viên',
+    label: <LabelTooltipForMenuItem label={'Thành viên'} />,
     icon: <span><i className="fa-solid fa-user"></i></span>,
     displayIndex: '3'
   },
   {
     key: '/manage-book',
     name: 'manage-book',
-    label: 'Quản lý sách',
+    label: <LabelTooltipForMenuItem label={'Quản lý sách'} />,
     icon: <span><i className="fa-solid fa-sliders"></i></span>,
     displayIndex: '4',
     children: [
@@ -56,7 +59,7 @@ export const routeComponents = [
         component: BookListView,
         key: '/manage-book/book',
         name: 'book',
-        label: 'Kho sách',
+        label: <LabelTooltipForMenuItem label={'Kho sách'} />,
         icon: <span><i className="fa-solid fa-book"></i></span>,
         displayIndex: '0',
       },
@@ -64,6 +67,41 @@ export const routeComponents = [
         component: ImportBookListView,
         key: '/manage-book/import-book',
         name: 'import-book',
+        label: <LabelTooltipForMenuItem label={'Nhập sách'} />,
+        icon: <span><i className="fa-solid fa-book-open"></i></span>,
+        displayIndex: '1',
+      },
+      // {
+      //   component: BookListView,
+      //   key: '/manage-book/warehouse-book',
+      //   name: 'warehouse-book',
+      //   label: 'Kho sách',
+      //   icon: <span><i className="fa-solid fa-book-bookmark"></i></span>,
+      //   displayIndex: '2',
+      // },
+    ]
+  },
+  {
+    key: '/manage-borrow',
+    name: 'manage-borrow',
+    title: 'Quản lý mượn trả',
+    label: <LabelTooltipForMenuItem label={'Quản lý mượn trả'} />,
+    icon: <span><i className="fa-solid fa-sliders"></i></span>,
+    displayIndex: '5',
+    children: [
+      {
+        component: BorrowListView,
+        title: 'Danh sách người mượn',
+        key: '/manage-borrow/borrow',
+        name: 'borrow',
+        label: <LabelTooltipForMenuItem label={'Danh sách người mượn'} />,
+        icon: <span><i className="fa-solid fa-book"></i></span>,
+        displayIndex: '0',
+      },
+      {
+        component: ImportBookListView,
+        key: '/manage-borrow/import-borrow',
+        name: 'import-borrow',
         label: 'Nhập sách',
         icon: <span><i className="fa-solid fa-book-open"></i></span>,
         displayIndex: '1',
@@ -81,7 +119,7 @@ export const routeComponents = [
   {
     key: '/config',
     name: 'config',
-    label: 'Cấu hình',
+    label: <LabelTooltipForMenuItem label={'Cấu hình'} />,
     icon: <span><i className="fa-solid fa-gear"></i></span>,
     displayIndex: '4',
     children: [
@@ -89,7 +127,7 @@ export const routeComponents = [
         component: MetaCataloListView,
         key: '/config/metacatalo',
         name: 'metacatalo',
-        label: 'Danh mục',
+        label: <LabelTooltipForMenuItem label={'Danh mục'} />,
         icon: <span><i className="fa-solid fa-bars"></i></span>,
         displayIndex: '0'
       },
